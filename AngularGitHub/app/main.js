1830857bb41c5ec8b212e265800455f527287636
Devin.Wirth <Devin.Wirth@e-hps.com>
2016-10-03 10:19:18 -0400

    Test Example
    First commit to repo

---
+++ AngularGitHub/app/main.js
@@ -0,0 +1,28 @@
+﻿
+
+(function() {
+    'use strict';
+
+    angular.module('app').controller('Main', GitHubCtrl);
+
+    function GitHubCtrl($scope, $http) {
+        $scope.getGitInfo = function() {
+            $scope.userNotFound = false;
+            $scope.loaded = false;
+
+            $http.get("https://api.github.com/users/" + $scope.username)
+                .success(function(data) {
+                    if (data.name == "") data.name = data.login;
+                    $scope.user = data;
+                    $scope.loaded = true;
+                })
+                .error(function() {
+                    $scope.userNotFound = true;
+                });
+            $http.get("https://api.github.com/users/" + $scope.username + "/repos").success(function(data) {
+                $scope.repos = data;
+                $scope.reposFound = data.length > 0;
+            });
+        };
+    }
+})();
